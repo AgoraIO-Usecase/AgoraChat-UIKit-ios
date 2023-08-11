0fdec8144c346276b3698bb28bfbbfa890986442
朱继超 <984065974@qq.com>
2023-08-11 15:01:30 +0800

    url preview

--- chat-uikit/Classes/Chat/MessageCell/EaseThreadCreateCell.m
+++ chat-uikit/Classes/Chat/MessageCell/EaseThreadCreateCell.m
@@ -461,6 +461,7 @@
     switch (aType) {
         case AgoraChatMessageTypeText:
         case AgoraChatMessageTypeExtCall:
+        case AgoraChatMessageTypeExtURLPreview:
             bubbleView = [[EMMsgTextBubbleView alloc] initWithDirection:self.direction type:aType viewModel:_viewModel];
             break;
         case AgoraChatMessageTypeImage:
