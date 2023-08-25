712e731eb84ce60cbbe1b19d9ea77f61e2dce351
朱继超 <984065974@qq.com>
2023-08-25 17:44:24 +0800

    quote clip

--- chat-uikit/Classes/Chat/MessageCell/MessageQuoteView.m
+++ chat-uikit/Classes/Chat/MessageCell/MessageQuoteView.m
@@ -40,7 +40,7 @@
 - (void)layoutSubviews {
     [super layoutSubviews];
     [self.content Ease_makeConstraints:^(EaseConstraintMaker *make) {
-        make.edges.Ease_equalTo(UIEdgeInsetsMake(6, 12, 6, 12));
+        make.edges.Ease_equalTo(UIEdgeInsetsMake(4, 12, 6, 12));
     }];
 }
 
