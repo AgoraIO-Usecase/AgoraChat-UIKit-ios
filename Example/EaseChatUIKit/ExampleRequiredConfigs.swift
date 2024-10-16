fedf98d7375e690bd6545b0c6725c73b7c03e133
朱继超 <984065974@qq.com>
2024-10-16 17:39:34 +0800

    Update ExampleRequiredConfigs.swift

--- Example/EaseChatUIKit/ExampleRequiredConfigs.swift
+++ Example/EaseChatUIKit/ExampleRequiredConfigs.swift
@@ -17,23 +17,23 @@ public class ExampleRequiredConfig {
     // Pass in the app key.
     // Get the app key by reference to the following URL:
     // https://docs.agora.io/en/agora-chat/get-started/enable#get-chat-project-information
-    static let appKey: String = "41117440#383391"
+    static let appKey: String = <#App Key#>
     
     // Use the user information of the current user object that conforms to the `EaseProfileProtocol` protocol to log in to chat_uikit.
     // For token generation, refer to the URL: https://docs.agora.io/en/agora-chat/get-started/enable?platform=ios#generate-a-user-token
-    static var chatToken: String = "007eJxTYLi3NvREfoexhFO100ZBwY3P06/Guh5s2+G5ehuTjUZRh5oCQ5phSrK5uUVSSkqymYlZYopFmpGZgaW5WXKiUYqBoWmyb+SntIZARoZbNQ3MjAysDIxACOKrMBhamKUYpxkY6CYbpqbpGhqmJusmJZsY6wJNNElMs0w2MUsyAgA1Lidj"
+    static var chatToken: String = <#chat token#>
     
     /// ``YourAppUser`` can be regarded as the user class in your App.
     public final class YourAppUser: NSObject, EaseProfileProtocol {
         
         /// Created user id.
-        public var id: String = "lxm"
+        public var id: String = <#user id#>
         
         public var remark: String = ""
         
         public var selected: Bool = false
         
-        public var nickname: String = "lxm001"
+        public var nickname: String = "tester001"
         
         public var avatarURL: String = "https://accktvpic.oss-cn-beijing.aliyuncs.com/pic/sample_avatar/sample_avatar_1.png"
         
