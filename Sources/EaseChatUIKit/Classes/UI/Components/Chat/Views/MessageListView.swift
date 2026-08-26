84a8f2961ead47423d3286b28ebc700373d7f2a9
朱继超 <984065974@qq.com>
2026-08-26 16:15:27 +0800

    Update MessageListView.swift

--- Sources/EaseChatUIKit/Classes/UI/Components/Chat/Views/MessageListView.swift
+++ Sources/EaseChatUIKit/Classes/UI/Components/Chat/Views/MessageListView.swift
@@ -446,31 +446,6 @@ public let MessageInputBarHeight = CGFloat(52)
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func processInputBarFirstResponder() {
-        self.inputBar.textViewFirstResponder = { [weak self] firstResponder in
-            guard let `self` = self else { return }
-            UIView.animate(withDuration: 0.25) {
-                let oldFrame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height-BottomBarHeight-MessageInputBarHeight)
-                self.messageList.frame = oldFrame
-                if firstResponder {
-                    self.messageList.frame = CGRect(x: 0, y: 0, width: self.messageList.frame.width, height: self.frame.height-self.inputBar.keyboardHeight-16-BottomBarHeight-(ScreenHeight <= 667 ? 28:0))
-                
-                } else {
-                    if self.inputBar.frame.height > MessageInputBarHeight {
-                        self.messageList.frame = CGRect(x: 0, y: 0, width: self.messageList.frame.width, height: self.frame.height-self.inputBar.frame.height-BottomBarHeight)
-                    } else {
-                        self.messageList.frame = oldFrame
-                    }
-                }
-                
-                let lastIndexPath = IndexPath(row: self.messages.count - 1, section: 0)
-                if lastIndexPath.row >= 0 {
-                    self.messageList.scrollToRow(at: lastIndexPath, at: .bottom, animated: false)
-                }
-            }
-        }
-    }
-    
     private func bottomMultiSelectedBarEvents(operation: MessageMultiSelectedBottomBarOperation) {
         for handler in self.eventHandlers.allObjects {
             handler.onMessageMultiSelectBarClicked(operation: operation)
